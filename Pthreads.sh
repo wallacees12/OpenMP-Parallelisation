a8acf1a55911c28d2ae8855a026733150f68961e
JoelA-jpg <hotmail.ja@gmail.com>
2025-02-25 14:53:43 +0100

    minor change to shellscript

--- Pthreads.sh
+++ Pthreads.sh
@@ -3,12 +3,17 @@
 read -p "Enter number of threads: " THREADS
 CC="gcc"
 CFLAGS="-O3 -ftree-vectorize -march=native -ffast-math"
-LDFLAGS="-lpthread"
+LDFLAGS="-lpthread -lm"
 NAME="galsim"
 TARGET="./Pthreads/${NAME}"
 SRCS="$TARGET.c" 
+CMP="./compare_gal_files/compare_gal_files"
+CMPSRC="$CMP.c"
 
 $CC $CFLAGS $SRCS -o $TARGET $LDFLAGS
+$CC  $CMPSRC -o $CMP $LDFLAGS
 
-time ./Pthreads/galsim 2000 ./input_data/ellipse_N_02000.gal 200 1e-5 0 $THREADS
+time $TARGET 2000 ./input_data/ellipse_N_02000.gal 200 1e-5 0 $THREADS
+
+#./$CMP result.gal ./
 
