a525f6063cd254eef001c95c24c27c4cc2561d66
Juan Rodriguez <juan.e.rodriguez.c.00@gmail.com>
2025-02-27 15:02:57 +0100

    First Try to merge galsimv7 and the parallel version. Doesn't pass the precision check. Good start point to build on.

--- Pthreads/galsim_balanced.c
+++ Pthreads/galsim_balanced.c
@@ -7,69 +7,67 @@
 #include <math.h>
 #include <pthread.h>
 
-typedef struct{
-  int start, end;
-  int N;
-  int n_threads;
-  double *m;
-  double *x;
-  // Now each thread has a local acceleration array
-  // double *a;
-  double G;
-  double eps;
-  pthread_mutex_t *mutex;
+typedef struct {
+    int start, end;
+    int N;
+    int n_threads;
+    double *m;
+    double *x;
+    double *y;
+    double eps;
 } ThreadData;
 
 
-static inline void
-Compute_ax_ay(int i, int j,
-              double *__restrict m,
-              double *__restrict x, double *a, double G, double eps);
+double *readData(const char *filename, int N); // Reads the initial condition data [x y m vx vy L]
+double *transform(const double *data, int N); // Allocates new 48N bytes in heap for the new order [m][x][y][vx][vy][L]
+void SaveLastStep(const char *filename, double *DATA, int N); // Saves final [x y m vx vy L]
 
-double *readData(const char *filename, int N);
+void *compute_fores(void *arg) {
+    ThreadData *data = (ThreadData *) arg;
+    int start = data->start, end = data->end;
+    int N = data->N;
+    int n_threads = data->n_threads;
+    double *m = data->m;
+    double *x = data->x;
+    double *y = data->y;
+    double eps = data->eps;
+    double *local_a = calloc(2 * N, sizeof(double));
+    double *ax = local_a;
+    double *ay = local_a + N;
+    double m_j, x_i, y_i;
+    int i, j;
+    register double F1, F2, dx, dy, dx2, dy2, R2, R, R3, invR3, Gx, Gy, m_i; // Frequently used in innermost loop
 
-double *transform(const double *data, int N);
-
-void SaveLastStep(const char *filename, double *DATA, int N); // Doesn't step out of a mountain
-
-void * compute_fores(void *arg){
-  // Unroll all of the data
-  ThreadData *data = (ThreadData *)arg;
-  int start = data->start, end = data->end;
-  int N = data->N;
-  int n_threads = data->n_threads;
-  double *m = data->m;
-  double *x = data->x;
-  // double *a = data->a; no longer needed as using local a
-  double G = data->G;
-  double eps = data->eps;
-  pthread_mutex_t *mutex = data->mutex;
-
-  // Local acceleration
-  double *local_a = calloc(2 * N, sizeof(double));
-
-  // Thread will only work on its allocated particles in range (start,end)
-  // printf("Thread started: start=%d, end=%d, N=%d\n", start, end, N);
-  for(int i = start; i < end; i += n_threads){
-    for(int j = i + 1; j < N; j++){
-        // Compute local i.e. ignores the mutex issues
-        // Debugging prints
-        // printf("Computing acceleration between i=%d, j=%d\n", i, j);
-        // fflush(stdout); // Ensure prints appear before a crash
-        Compute_ax_ay(i, j, m, x, local_a, G, eps);
+    for (i = start; i < end; i += n_threads) {
+        x_i = x[i]; y_i = y[i]; m_i = m[i];
+        F1 = ax[i]; F2 = ay[i];
+#pragma GCC ivdep
+        for (j = i + 1; j < N; j++) {
+            dx = x[j] - x_i; dy = y[j] - y_i;
+            dx2 = dx * dx; dy2 = dy * dy;
+            R2 = dx2 + dy2;
+            R = sqrt(R2) + eps;
+            R3 = R * R;
+            R3 *= R;
+            invR3 = 1.0 / R3;
+            Gx = Gy = invR3;
+            Gx *= dx; Gy *= dy;
+            ax[j] -= Gx * m_i; ay[j] -= Gy * m_i;
+            m_j = m[j];
+            F1 += Gx * m_j; F2 += Gy * m_j;
+        }
+        ax[i] = F1; ay[i] = F2;
     }
-    // printf("Thread finished: start=%d, end=%d\n", start, end);
-  }
-  return local_a; // threaded function returns pointer, can be interpreted as VOID * we will cast
+    return local_a;
 }
-
 
 
 int main(int argc, char *argv[]) {
 
     if (argc != 7) {
         printf("Incorrect usage: ./gaslsim N filname nsteps delta_t graphics n_threads\n");
-        return 1;}
+        return 1;
+    }
 
     int N = atoi(argv[1]);
     const char *filename = argv[2];
@@ -82,59 +80,56 @@ int main(int argc, char *argv[]) {
     printf("n-threads = %d\n", n_threads);
 
     // Same as Vanilla
-    double *data = readData(filename, N); // x0 y0 m0 vx0 vy0 L0
-    double *DATA = transform(data, N); // [m0...mN-1] [x0 y0 x1 y1 ,....] [vx0 vy0, ...] [L0 L1,...]
-    double *a = calloc(2 * N, sizeof(double)); // Now our Global acceleration
-    double *m = DATA; // -> DATA[0] ; m[i]
-    double *x = DATA + N; // -> x [x0, y0] -> x_i = x[2*i], y_i = x[2*i+1]
-    double *v = DATA + 3 * N;
-
-    // Initalise threads using pthreads
+    double *data = readData(filename, N);
+    double *DATA = transform(data, N);
+    double *m = DATA;
+    double *x = DATA + N;
+    double *y = DATA + 2*N;
+    double *u = DATA + 3*N;
+    double *v = DATA + 4 * N;
+    double *a = calloc(2 * N, sizeof(double));
+    double *ax = a;
+    double *ay = a + N;
     pthread_t threads[n_threads];
     ThreadData thread_data[n_threads];
-    // pthread_mutex_t mutex =  PTHREAD_MUTEX_INITIALIZER; No longer using this! We are calculating local then merge
 
-    // Have to compute chunk size for each thread
-    int chunk_size = N / n_threads;
-
-    int i, j, n;
+    int i, t, n;
     n = 0;
 
 
-    for(int n = 0; n < n_steps; n++) {
-
-        for(int t=0; t < n_threads; t++){
+    for (n = 0; n < n_steps; n++) {
+        memset(a, 0, 2 * N * sizeof(double));
+        for (t = 0; t < n_threads; t++) {
             thread_data[t].start = t; // Start each thread at its index
             thread_data[t].end = N; // End at N particles
             thread_data[t].N = N;
             thread_data[t].m = m;
             thread_data[t].x = x;
-            thread_data[t].G = G;
+            thread_data[t].y = y;
             thread_data[t].eps = eps;
             thread_data[t].n_threads = n_threads;
             pthread_create(&threads[t], NULL, compute_fores, &thread_data[t]);
         }
-
-        memset(a, 0, 2 * N * sizeof(double));
         // Must wait for all our threads to come back to us for this step iteration
-        for(int t = 0; t < n_threads; t++){
-          double *local_a = NULL;
-          pthread_join(threads[t], (void**)&local_a); // Retrieve our calculations
-          for(int i = 0; i < 2 * N; i++){
-            a[i] += local_a[i];
-          }
-          free(local_a);
+        for (t = 0; t < n_threads; t++) {
+            double *local_a = NULL;
+            pthread_join(threads[t], (void **) &local_a);
+            double *ax_local = local_a;
+            double *ay_local = local_a + N;
+            for (i = 0; i < N; i++) {
+                ax[i] += ax_local[i];
+                ay[i] += ay_local[i];
+            }
+            free(local_a);
         }
-        // We can definitely unroll this loop explicitly but the fucking compiler better
-        // not be as lazy as me
-        for (i = 0; i < 2 * N; i += 2) {
-            v[i] += a[i] * dt; // Update x-velocity
-            x[i] += v[i] * dt; // Update x-position
-            v[i + 1] += a[i + 1] * dt; // Update y-velocity
-            x[i + 1] += v[i + 1] * dt; // Update y-position
+
+        for (i = 0; i < N; i++) {
+            v[i] += ax[i] * dt; // Update x-velocity
+            x[i] += u[i] * dt; // Update x-position
+            v[i] += ay[i] * dt; // Update y-velocity
+            y[i] += v[i] * dt; // Update y-position
         }
-        // Let's not even think about how this guy can be changed...
-        // memset(a, 0, 2 * N * sizeof(double)); // At least we don't store accelerations in a matrix, that shit cringe
+
     }
     SaveLastStep("result_balanced.gal", DATA, N);
     free(a);
@@ -150,7 +145,7 @@ static inline void
 Compute_ax_ay(int i, int j, double *__restrict m, double *__restrict x, double *a, double G, double eps) {
     double dx, dy, R2, R, R3, invR3, Gx, Gy;
     // Use particle positions: x[2*i], x[2*i+1] for particle i
-    dx = x[2 * j]     - x[2 * i];
+    dx = x[2 * j] - x[2 * i];
     dy = x[2 * j + 1] - x[2 * i + 1];
 
     R2 = dx * dx + dy * dy;
@@ -162,10 +157,10 @@ Compute_ax_ay(int i, int j, double *__restrict m, double *__restrict x, double *
     Gy = G * invR3 * dy;
 
     // For particle i, add contribution from j
-    a[2 * i]     += Gx * m[j];
+    a[2 * i] += Gx * m[j];
     a[2 * i + 1] += Gy * m[j];
     // For particle j, subtract contribution from i (Newton's third law)
-    a[2 * j]     -= Gx * m[i];
+    a[2 * j] -= Gx * m[i];
     a[2 * j + 1] -= Gy * m[i];
 }
 
@@ -194,16 +189,15 @@ double *readData(const char *filename, int N) {
 double *transform(const double *data, int N) {
     double *DATA = malloc(6 * N * sizeof(double));
     for (int i = 0; i < N; i++) {
-        DATA[i] = data[6 * i + 2]; // m0, m1, ..., m(N-1),
-        DATA[N + 2 * i] = data[6 * i]; // m(N-1), x0,
-        DATA[N + 2 * i + 1] = data[6 * i + 1]; // m(N-1), x0, y0
-        DATA[3 * N + 2 * i] = data[6 * i + 3];
-        DATA[3 * N + 2 * i + 1] = data[6 * i + 4];
+        DATA[i] = data[6 * i + 2]; // Since this function is called only once defining idx = 6*i has little effect
+        DATA[N + i] = data[6 * i];
+        DATA[2 * N + i] = data[6 * i + 1];
+        DATA[3 * N + i] = data[6 * i + 3];
+        DATA[4 * N + i] = data[6 * i + 4];
         DATA[5 * N + i] = data[6 * i + 5];
     }
     return DATA;
 }
-
 
 void SaveLastStep(const char *filename, double *DATA, int N) {
     FILE *file = fopen(filename, "wb");
@@ -211,16 +205,20 @@ void SaveLastStep(const char *filename, double *DATA, int N) {
         printf("Error opening file %s\n", filename);
         exit(1);
     }
+    // Define pointers to segments in DATA for more readable write instructions
     double *m = DATA;
     double *x = DATA + N;
-    double *v = DATA + 3 * N;
+    double *y = DATA + 2 * N;
+    double *u = DATA + 3 * N;
+    double *v = DATA + 4 * N;
     double *L = DATA + 5 * N;
+
     for (int i = 0; i < N; i++) {
-        fwrite(&x[i * 2], sizeof(double), 1, file); // &x[] pointer to accessed value
-        fwrite(&x[i * 2 + 1], sizeof(double), 1, file);
+        fwrite(&x[i], sizeof(double), 1, file); // Write 1 double for each quantity
+        fwrite(&y[i], sizeof(double), 1, file);
         fwrite(&m[i], sizeof(double), 1, file);
-        fwrite(&v[i * 2], sizeof(double), 1, file);
-        fwrite(&v[i * 2 + 1], sizeof(double), 1, file);
+        fwrite(&u[i], sizeof(double), 1, file);
+        fwrite(&v[i], sizeof(double), 1, file);
         fwrite(&L[i], sizeof(double), 1, file);
     }
     fclose(file);
