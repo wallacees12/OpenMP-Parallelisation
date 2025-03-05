0737cb9a483e82b63b3646ddc01601184405cdd4
JoelA-jpg <hotmail.ja@gmail.com>
2025-03-05 15:45:14 +0100

    Started updated omp

--- run_and_compare.sh
+++ run_and_compare.sh
@@ -4,13 +4,13 @@ read -p "Enter number of threads: " THREADS
 CC="gcc"
 CFLAGS="-O3 -ftree-vectorize -march=native -ffast-math"
 LDFLAGS="-lpthread -lm"
-NAME="galsim_balanced_row"
-DIR="Pthreads"
+NAME="galsim_OpenMP"
+DIR="Openmp/Joel"
 TARGET="./$DIR/${NAME}"
 SRCS="$TARGET.c" 
 CMP="./compare_gal_files/compare_gal_files"
 CMPSRC="$CMP.c"
-CMPFILE="result_balanced_row.gal"
+CMPFILE="galsim_OpenMP.gal"
 
 $CC $CFLAGS $SRCS -o $TARGET $LDFLAGS
 $CC  $CMPSRC -o $CMP $LDFLAGS
